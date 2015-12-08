88a703003716d2ba770c2df0daa98d01ff0098f4
Eloylp <eloy@sandboxwebs.com>
2015-12-08 18:48:09 +0100

    first views with symfony

---
+++ src/Mo/MainWebsiteBundle/Controller/MainWebSiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mo\MainWebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainWebSiteController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@MoMainWebsite/index.html.twig');
+    }
+}
