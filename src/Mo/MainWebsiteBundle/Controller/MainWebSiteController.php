cbd4942ad2e11d4a358d6dcfc8326dd73245f3fd
Eloylp <eloy@sandboxwebs.com>
2015-12-20 18:17:54 +0100

    Added translation support and basic demo usign translation service.

--- src/Mo/MainWebsiteBundle/Controller/MainWebSiteController.php
+++ src/Mo/MainWebsiteBundle/Controller/MainWebSiteController.php
@@ -3,12 +3,21 @@
 namespace Mo\MainWebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainWebSiteController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@MoMainWebsite/index.html.twig');
+    }
+
+    public function translationTestAction()
+    {
+        $welcome = $this->get('translator')->trans('mo.mainwebsite.welcome');
+
+        return new Response($welcome);
+
     }
 
     public function contactAction()
